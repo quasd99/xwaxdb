baebb7d18f4496a646b484a7d44dbda7bab71952
Frank Nießen <niessen.frank@gmx.de>
2016-09-27 20:23:53 +0200

    settings dialog added

---
+++ src/uisettingsdialog.cpp
@@ -0,0 +1,40 @@
+#include "uisettingsdialog.h"
+
+UiSettingsDialog::UiSettingsDialog(BaseObjectType* cobject,
+                                   const Glib::RefPtr<Gtk::Builder>& refGlade)
+: Gtk::Dialog(cobject),
+  builder(refGlade)
+{
+	set_title("xwaxdb settings");
+	set_size_request(600,300);
+	
+	builder->get_widget("cbtn_gmbrc", cbtn_gmbrc);
+	builder->get_widget("rbtn_std_gmbrc", rbtn_std_gmbrc);
+	builder->get_widget("rbtn_custom_gmbrc", rbtn_custom_gmbrc);
+	builder->get_widget("rbtn_custom_gmbrc", rbtn_custom_gmbrc);
+	builder->get_widget("entry_custom_gmbrc", entry_custom_gmbrc);
+	builder->get_widget("cbtn_af", cbtn_af);
+	builder->get_widget("cbtn_pls", cbtn_pls);
+	builder->get_widget("entry_crates_path", entry_crates_path);
+	builder->get_widget("cbtn_bpm_scan", cbtn_bpm_scan);
+	
+	add_button(Gtk::Stock::APPLY, 1);
+	add_button(Gtk::Stock::CANCEL, 0);
+}
+
+void
+UiSettingsDialog::exec()
+{
+	int status = run();
+	switch (status)
+	{
+		case 0:
+			// cancel
+			break;
+		case 1:
+			// TODO: Save
+			break;
+	}
+}
+
+
---
+++ src/uisettingsdialog.h
@@ -0,0 +1,35 @@
+#ifndef UISETTINGSDIALOG_H
+#define UISETTINGSDIALOG_H
+
+#include <gtkmm-3.0/gtkmm.h>
+
+class UiSettingsDialog : public Gtk::Dialog
+{
+  
+public:
+  UiSettingsDialog(BaseObjectType* cobject, 
+                   const Glib::RefPtr<Gtk::Builder>& refGlade);
+
+  /**
+   * @brief Init the dialog with current settings and exec run
+   */
+  void exec();
+protected:
+  Glib::RefPtr<Gtk::Builder> builder;
+
+  // glade ui reference-pointer
+  Gtk::CheckButton* cbtn_gmbrc;
+  Gtk::RadioButton* rbtn_std_gmbrc;
+  Gtk::RadioButton* rbtn_custom_gmbrc;
+  Gtk::Entry*       entry_custom_gmbrc;
+  
+  Gtk::CheckButton* cbtn_af;
+  Gtk::CheckButton* cbtn_pls;
+  Gtk::Entry*       entry_crates_path;
+  
+  Gtk::CheckButton* cbtn_bpm_scan;
+  
+private:
+};
+
+#endif // UISETTINGSDIALOG_H
